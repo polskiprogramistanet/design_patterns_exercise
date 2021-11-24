b3225749294da84e37361f7c4827b73541f0229e
polskiprogramistanet <rav@polskiprogramista.net>
2021-11-24 07:37:24 +0100

    Dodaj pliki projektów.

---
+++ design_patterns_exercise/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace design_patterns_exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
