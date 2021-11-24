c4a8976464039b391e8e772df2aebd4adb60fcb0
polskiprogramistanet <rav@polskiprogramista.net>
2021-11-24 09:02:54 +0100

    before all refactoring

--- design_patterns_exercise/Employee.cs
+++ design_patterns_exercise/Employee.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace design_patterns_exercise
 {
-    class Employee
+    public class Employee
     {
+        public EmployeeType Type { get;private set; }
+
+        public Employee(EmployeeType employeeType)
+        {
+            this.Type = employeeType;
+        }
+
+        public double CalculateNewSalary(double @base, double sumOfIncomes, double employeeIncome, double importantCalculationFactor)
+        {
+
+            switch (Type)
+            {
+                case EmployeeType.CEO:
+
+                    return @base + sumOfIncomes * 0.2 + 200 + importantCalculationFactor * employeeIncome * 2;
+                case EmployeeType.Sales:
+
+                    return @base + sumOfIncomes * 0.1 + importantCalculationFactor * employeeIncome;
+                case EmployeeType.Worker:
+
+                    return @base + sumOfIncomes * importantCalculationFactor;
+                default:
+                    return 0.00;
+            }
+        }
     }
 }
