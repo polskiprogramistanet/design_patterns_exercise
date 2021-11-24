e930dbaa2f5a49a8562dae3b95716cff98cabffb
polskiprogramistanet <rav@polskiprogramista.net>
2021-11-24 08:53:30 +0100

    before

---
+++ design_patterns_exercise/EmployeeType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace design_patterns_exercise
+{
+    public enum EmployeeType
+    {
+        CEO,
+        Sales,
+        Worker
+    }
+}
